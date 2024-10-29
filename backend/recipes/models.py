aa18106c7497bbd69ec4a2235d93f2fe9ba42ef6
Darian Cusco <cusco.d@gmail.com>
2024-10-29 15:53:58 -0400

    tweaked recipes models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    carbon_emission = models.DecimalField(max_digits=5, decimal_places=2)  # Carbon emision measures in kg
+    carbon_emission = models.DecimalField(max_digits=5, decimal_places=2, default=0)  # Carbon emision measures in kg
 
     def __str__(self):
         return self.name
@@ -11,7 +11,7 @@ class Ingredient(models.Model):
 class Recipe(models.Model):
     title = models.CharField(max_length=100)
     ingredients = models.ManyToManyField(Ingredient, related_name='recipe')
-    total_emission = models.DecimalField(max_digits=8, decimal_places=2)  #Total emission of a recipe
+    total_emission = models.DecimalField(max_digits=8, decimal_places=2, default=0)  #Total emission of a recipe
 
     def __str__(self):
         return self.title
