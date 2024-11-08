8aa69cef2443b2e16f65979a2499ea32515f96c9
Andrew Goldstein <fablefox5@gmail.com>
2024-11-07 22:46:02 -0500

    temporarily comment out non-functional url patterns

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('logoout', views.logout_view),
+    #path('home/', views.home, name='home'),
+    #path('logoout', views.logout_view),
     path('recipes/', views.RecipeCreate.as_view(), name='recipe-create-view'),
     path('ingredient/', views.IngredientCreate.as_view(), name='ingredient-create-view'),
     path('category/', views.CategoryCreate.as_view(), name='category-create-view'),
