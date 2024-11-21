16735b2f5a04748e1e0bfdf5578c6f9bbb9460be
dmorsecode <morsecoding@protonmail.com>
2024-11-21 15:39:18 -0500

    Added category selector to recipe creator.

--- frontend/src/components/Recipe/RecipeInfo.tsx
+++ frontend/src/components/Recipe/RecipeInfo.tsx
@@ -1,7 +1,8 @@
 ﻿import { useState } from 'react'
 
-function RecipeInfo({ title, description, prep, cook, servings, editable }: {
+function RecipeInfo({ title, category, description, prep, cook, servings, editable }: {
   title: string,
+  category: string,
   description: string,
   prep: number,
   cook: number,
@@ -13,12 +14,22 @@ function RecipeInfo({ title, description, prep, cook, servings, editable }: {
   const [prepView, setPrep] = useState(prep)
   const [cookView, setCook] = useState(cook)
   const [servingsView, setServings] = useState(servings)
+  const [categoryView, setCategory] = useState(category)
 
   if (editable) return (
     <div className="flex flex-col w-full items-center justify-center p-2 h-full">
-      <input type="text" placeholder="Recipe Name"
-             onChange={(e) => setTitle(e.currentTarget.value)}
-             className={`text-2xl text-center border-2 border-neutral-400 border-dashed p-2 rounded-sm ${titleView ? '' : 'opacity-50'}`} />
+      <div className="flex gap-x-4">
+        <input type="text" placeholder="Recipe Name"
+               onChange={(e) => setTitle(e.currentTarget.value)}
+               className={`text-2xl text-center border-2 border-neutral-400 border-dashed p-2 rounded-sm ${titleView ? '' : 'opacity-50'}`} />
+        <select defaultValue="Category" className="select select-bordered h-full" id="categorySelector"
+                onChange={(e) => setCategory(e.target.value)}>
+          <option disabled>Category</option>
+          <option>Breakfast</option>
+          <option>Lunch</option>
+          <option>Dinner</option>
+        </select>
+      </div>
       <div className="flex-col items-center hidden md:flex h-full w-full">
         <div className="divider divider-neutral opacity-50 w-full m-y-2 hidden lg:flex" />
         <input type="text" placeholder="Write a description for your recipe here."
@@ -38,7 +49,7 @@ function RecipeInfo({ title, description, prep, cook, servings, editable }: {
         <div className="flex basis-1/3 gap-2 items-center justify-center">
           <p>Prep Time:</p>
           <input type="text" placeholder="0"
-                  onChange={(e) => setCook(parseInt(e.currentTarget.value))}
+                 onChange={(e) => setCook(parseInt(e.currentTarget.value))}
                  className={`w-1/4 text-center border-2 border-neutral-400 border-dashed px-2 rounded-sm ${cookView ? '' : 'opacity-50'}`} />
           <p>min</p>
         </div>
@@ -46,7 +57,7 @@ function RecipeInfo({ title, description, prep, cook, servings, editable }: {
         <div className="flex basis-1/3 gap-2 items-center justify-center">
           <p>Prep Time:</p>
           <input type="text" placeholder="0"
-                  onChange={(e) => setServings(parseInt(e.currentTarget.value))}
+                 onChange={(e) => setServings(parseInt(e.currentTarget.value))}
                  className={`w-1/4 text-center border-2 border-neutral-400 border-dashed px-2 rounded-sm ${servingsView ? '' : 'opacity-50'}`} />
           <p>min</p>
         </div>
