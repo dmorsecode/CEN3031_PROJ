8526b71256c850588a0fe08dca8b54918936393b
dmorsecode <morsecoding@protonmail.com>
2024-12-02 14:51:45 -0500

    Fix failing frontend test after adding google sign in button.

--- frontend/src/components/test.tsx
+++ frontend/src/components/test.tsx
@@ -2,15 +2,20 @@ import { render, screen } from '@testing-library/react'
 
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 describe('<App />', () => {
   it('should render the App', () => {
-    const { container } = render(<App />, {wrapper: BrowserRouter})
+    const { container } = render(
+      <GoogleOAuthProvider clientId="44278308785-cmoev9iv03ho0meol66tslpkk2nrk7dk.apps.googleusercontent.com">
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </GoogleOAuthProvider>)
 
     expect(
-      screen.getByRole('heading', {
-        name: /Welcome to Your Food Carbon Emission Tracker/i,
-        level: 1
+      screen.getByRole('button', {
+        name: /SIGN IN WITH GOOGLE/i,
       })
     ).toBeInTheDocument()
 
